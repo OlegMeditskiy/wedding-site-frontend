e9a7ebecdb32bff44225c17076ba0e11e91aaf66
OlegMeditskiy <olegmeditsky@gmail.com>
2020-07-18 22:48:50 +0300

    dev

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8909;
+const wakeUpDyno = require("wokeDyno.js");
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
+const DYNO_URL = "http://daria-alexander-wedding.herokuapp.com/";
 app.use(favicon(__dirname + '/build/favicon.ico'));
 
 //здесь наше приложение отдаёт статику
@@ -20,4 +22,6 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+app.listen(port,()=>{
+    wakeUpDyno(DYNO_URL);
+});
