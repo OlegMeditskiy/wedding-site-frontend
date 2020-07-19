18d954b202adde072734bf7907a5e78cdcf44c10
OlegMeditskiy <olegmeditsky@gmail.com>
2020-07-19 14:10:12 +0300

    dev

--- src/MainPage/parts/DressCode.js
+++ src/MainPage/parts/DressCode.js
@@ -9,14 +9,16 @@ const DressCode = (props)=>{
         return(
             <div>
                 <Row>
-                    <Col id={"dressCodeImage1"} className={"text-center"} lg={6} xl={6}>
-                        <Image style={{"max-width":"100%"}} src={dressCode}/>
-                    </Col>
-                    <Col style={{"padding":"30px 10px 0px 30px"}} className={"text-center site-text"} lg={6}  xl={6}>
-                        <h1 style={{"marginBottom":"50px"}} className={"text-center title"}><span className={"headline"}>{translation.menu.dressCode}</span></h1>
-                        <div>Мы будем очень признательны, если вы используете цвета нашей свадьбы в своих образах
+                    {/*<Col id={"dressCodeImage1"} className={"text-center"} lg={6} xl={6}>*/}
+                    {/*    <Image style={{"max-width":"100%"}} src={dressCode}/>*/}
+                    {/*</Col>*/}
+                    <Col style={{"padding":"30px 10px 0px 30px"}} className={"text-center site-text"} lg={12}  xl={12}>
+                        <h1  className={"text-center title"}><span className={"headline"}>{translation.menu.dressCode}</span></h1>
+                        <div style={{"padding":"30px 0px"}}>
+                            <p>Мы будем очень признательны, если вы используете цвета нашей свадьбы в своих образах</p>
+                            <Image className={"text-center"} id={"dressCodeImage2"} style={{"max-width":"100%","display":"inline-block"}} src={dressCode}/>
+
                         </div>
-                        <Image className={"text-center"} id={"dressCodeImage2"} style={{"max-width":"100%"}} src={dressCode}/>
                     </Col>
                 </Row>
 
@@ -26,7 +28,7 @@ const DressCode = (props)=>{
         )
     }
     return(
-        <div id={"dressCode"} style={{"padding":"0px"}} className={"mainPageBlock evenBlock"}>
+        <div id={"dressCode"} style={{"padding":"0px"}} className={"mainPageBlock odBlock"}>
             {returnRead()}
         </div>
     )
