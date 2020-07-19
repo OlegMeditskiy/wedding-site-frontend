8532ea79fbfb326584ca4689762dd931837a7a1c
OlegMeditskiy <olegmeditsky@gmail.com>
2020-07-19 16:36:25 +0300

    dev

--- src/MainPage/parts/Navigation.js
+++ src/MainPage/parts/Navigation.js
@@ -8,11 +8,11 @@ const Navigation = () =>{
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="justify-content-center w-100">
-                    <Nav.Link  href="#aboutUs">{translation.menu.aboutUs}</Nav.Link>
+                    <Nav.Link  href="#home">Главная</Nav.Link>
                     <Nav.Link href="#ourStory">{translation.menu.ourStory}</Nav.Link>
                     <Nav.Link href="#program">{translation.menu.program}</Nav.Link>
                     <Nav.Link href="#dressCode">{translation.menu.dressCode}</Nav.Link>
-                    <Nav.Link href="#invitation">{translation.menu.invitation}</Nav.Link>
+                    <Nav.Link href="#invitation">Я приду</Nav.Link>
                     {/*<Nav.Link href="#place">{translation.menu.place}</Nav.Link>*/}
                 </Nav>
 
