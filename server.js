dba58726b770c074ad0cea7a21461e13ad9eddf3
OlegMeditskiy <43202655+OlegMeditskiy@users.noreply.github.com>
2020-07-19 11:06:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const port = process.env.PORT || 8909;
 const wakeDyno = require("woke-dyno");
 const DYNO_URL = "http://daria-alexander-wedding.herokuapp.com/";
+const DYNO_URL2 = "http://daria-alexander-wedding.herokuapp.com/admin";
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
@@ -23,4 +24,5 @@ app.get('/*', function (req, res) {
 });
 app.listen(port,()=>{
     wakeDyno(DYNO_URL).start();
+    wakeDyno(DYNO_URL2).start();
 });
